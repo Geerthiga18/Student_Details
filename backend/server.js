7632d250d390713081ec67c1514a2402b75d3ffa
Geerthiga18 <geerthishan@gmail.com>
2025-03-16 17:26:14 +0530

    Updated files with necessary changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const session = require('express-session');
-const passport = require('passport');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
@@ -20,17 +19,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
-
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'defaultsecret',
-    resave: false,
-    saveUninitialized: false
-}));
-
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -57,5 +45,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
